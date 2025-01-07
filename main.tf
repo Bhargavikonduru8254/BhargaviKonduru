5bef0b6793b486b086b90f677cc747c3ce8e3f4a
Bhargavikonduru8254 <bhargavikonduru8254@gmail.com>
2025-01-07 23:04:58 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,76 +1,90 @@
 
-//create new resource group using terraform
+/*//create new resource group using terraform
 resource "azurerm_resource_group" "tpt_batch_01" {
 name = "rg-training-tpt-02"
 location = "central us"
 }
 
-# Virtual Network
-resource "azurerm_virtual_network" "dev_virtual_network_01" {
-name                =var.vnet_name
-resource_group_name = azurerm_resource_group.tpt_batch_01.name
-location            = azurerm_resource_group.tpt_batch_01.location
-address_space       = var.vnet_address_space
-tags                = var.tags
+*/
+
+resource "azurerm_resource_group" "test" {
+  name     = "test-rg"
+  location = "West Europe"
 }
 
-# Subnet 1
-resource "azurerm_subnet" "my_subnet_01" {
-  name                 = "subnet-01"
-  resource_group_name  = azurerm_resource_group.tpt_batch_01.name
-  virtual_network_name = azurerm_virtual_network.dev_virtual_network_01.name
-  address_prefixes     = ["10.0.0.0/24"]
+resource "azurerm_storage_account" "test" {
+  name                     = "teststgaccount"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
-# Subnet 2
-resource "azurerm_subnet" "my_subnet_02" {
-  name                 = "subnet-02"
-  resource_group_name  = azurerm_resource_group.tpt_batch_01.name
-  virtual_network_name = azurerm_virtual_network.dev_virtual_network_01.name
-  address_prefixes     = ["10.0.1.0/24"]
-  }
 
-# Subnet 3
-resource "azurerm_subnet" "my_subnet_03" {
-  name                 = "subnet-03"
-  resource_group_name  = azurerm_resource_group.tpt_batch_01.name
-  virtual_network_name = azurerm_virtual_network.dev_virtual_network_01.name
+resource "azurerm_storage_account" "test1" {
+  name                     = "teststgaccount2"
+  resource_group_name      = azurerm_resource_group.test.name
+  location                 = azurerm_resource_group.test.location
+  account_tier             = "Premium"
+  account_replication_type = "ZRS"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "virtnetname"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "subnet01" {
+  name                 = "subnetname"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_storage_account" "my_storage_01"{
-name    ="storageaccount01"
-resource_group_name = azurerm_resource_group.tpt_batch_01.name
-location =azurerm_resource_group.tpt_batch_01.location
-account_tier = "standard"
-account_replication_type  = "LRS"
 
-
-
-#Create private endpoint
-resource "azurerm_private_endpoint" "Private_endpoint" {
-  name = "private-endpoint01"
-  resource_group_name = azurerm_resource_group.tpt_batch_01.name
-  location = azurerm_resource_group.tpt_batch_01.location
-  subnet_id = azurerm_subnet.my_subnet_02.id
-  
-}
-}
-resource "azurerm_storage_account" "my_storage_02"{
-name    ="storageaccount02"
-resource_group_name = azurerm_resource_group.tpt_batch_01.name
-location =azurerm_resource_group.tpt_batch_01.location
-account_tier = "Premium"
-account_replication_type  = "ZRS"
-
-#Create private endpoint
-resource "azurerm_private_endpoint" "Private_endpoint1" {
-  name = "private-endpoint02"
-  resource_group_name = azurerm_resource_group.tpt_batch_01.name
-  location = azurerm_resource_group.tpt_batch_01.location
-  storage_sub-resource ="blob"
-  subnet_id = azurerm_subnet.my_subnet_03.id
-  
+resource "azurerm_subnet" "subnet02" {
+  name                 = "subnetname2"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.3.0/24"]
 }
 
+
+resource "azurerm_subnet" "subnet03" {
+  name                 = "subnetname3"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.4.0/24"]
 }
+
+resource "azurerm_private_endpoint" "test" {
+  name                = "example-endpoint"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  subnet_id           = azurerm_subnet.subnet02.id
+
+  private_service_connection {
+    name                           = "example-privateserviceconnection"
+    private_connection_resource_id = azurerm_storage_account.test.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  }
+
+resource "azurerm_private_endpoint" "test1" {
+  name                = "example-endpoint"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  subnet_id           = azurerm_subnet.subnet03.id
+
+  private_service_connection {
+    name                           = "example-privateserviceconnection"
+    private_connection_resource_id = azurerm_storage_account.test1.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  }  
